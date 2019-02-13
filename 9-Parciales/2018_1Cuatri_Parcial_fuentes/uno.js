75b82e41b4dce0d0d923938f4611bd6adcb92bb1
JavierSchmidt <47337671+JavierSchmidt@users.noreply.github.com>
2019-02-13 20:17:04 -0300

    Add files via upload

--- 9-Parciales/2018_1Cuatri_Parcial_fuentes/uno.js
+++ 9-Parciales/2018_1Cuatri_Parcial_fuentes/uno.js
@@ -1,5 +1,37 @@
 
 function mostrar()
 {
+var nombre1;
+var nombre2;
+var edad1;
+var edad2;
+var sumaedad=edad1+edad2;
+
+	nombre1=elNombre1.value;
+	nombre2=elNombre2.value;
+	edad1=laEdad1.value;
+	edad2=laEdad2.value;
+
+	edad1=parseInt(edad1);
+	edad2=parseInt(edad2);
+
+	alert("Ingrese el nombre del primero");
+
+	alert("Ingrese la edad del primero");
+
+	alert("Ingrese el nombre del segundo");
+
+	alert("Ingrese la edad del segundo");
+
+		alert("Ustedes son" Nombre1 "y" nombre2);
+		alert("sus edades son" edad1 "y" edad2);
+		alert("la suma de las edades es" sumaedad);
+
+
+
+
+
+
+
 
 }
